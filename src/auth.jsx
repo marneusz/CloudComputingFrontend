1aabb9fbe3fc967357e769778e29a6a7a0381c2b
marneusz <290639@student.mini.pw.edu.pl>
2021-06-06 23:50:09 +0200

    basic register

--- src/auth.jsx
+++ src/auth.jsx
@@ -1,8 +1,45 @@
-import React from 'react' 
+import React, { useState } from 'react' 
+// npm i amazon-cognito-identity-js
+import { CognitoUserPool } from 'amazon-cognito-identity-js'
+import conf from "./conf.json";
 
 function Auth(){ 
-    return (<div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const poolData = {
+        UserPoolId: conf.cognito.userPoolId,
+        ClientId: conf.cognito.userPoolClientId
+    };
+
+    const UserPool = new CognitoUserPool(poolData);
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+        UserPool.signUp(email, password, [], null, (err, data) => {
+                if (err) console.error(err);
+                console.log(data);
+            }
+        );
+    };
+
+    return (
+    <div>
         Here goes authentication.
-    </div>)
+        <form onSubmit={onSubmit}>
+            <input 
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+            />
+            <input 
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+            />
+
+            <button type='submit'>Sign Up!</button>
+        </form>
+    </div>
+    )
 }
 export default Auth
